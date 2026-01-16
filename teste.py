87129f1b6e9f059f48352681d7b69eceb8c73297
SanmyrB <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>
2026-01-16 11:18:03 -0400

    Destilação Finalizada e Inicio do Teste de Hipóteses

--- teste.py
+++ teste.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.optimize import minimize
+import matplotlib.pyplot as plt
 
 Bcaldo = 17.08
 PolCaldo = 14.21
@@ -49,7 +50,7 @@ vazMosto = MistResult["Tanque de Mistura"]["Vazão do Mosto (ton/h)"]
 brixMosto = MistResult["Tanque de Mistura"]["Pureza do Mosto (%)"]
 purzMosto = MistResult["Tanque de Mistura"]["Pureza do Mosto (%)"]
 
-def fermentacao(vazMosto, brixMosto, purzMosto, convert = 0.8):
+def fermentacao(vazMosto, brixMosto, purzMosto, convert = 0.9):
 
     # Vazão de Sacarose Disponível (kg/h)
     vazSac_kg = (vazMosto * brixMosto / 100 * purzMosto / 100) * 1000
@@ -107,74 +108,102 @@ vazVinho_m3 = ferment["Fermentação"]["Vazão de do Vinho Fermentado (m³/h)"]
 vazEt_m3 = ferment["Fermentação"]["Vazão de Etanol Disponível (m³/h)"]
 concentEt = ferment["Fermentação"]["Concentração de Etanol"]
 
-def Coluna_PNL (nome, vazVinho_m3, fracEt_in, fracEt_Topo, fracEt_Liq, fracEt_Fundo):
+def Coluna_MN(nome, vaz_in, fracEt_in, fracEt_Topo, fracEt_Liq, fracEt_Fundo, disponiagric, disponiClima, disponiIndust):
 
-    x = np.array([fracEt_Topo, fracEt_Liq, fracEt_Fundo])
+    # Balanço de Massa
+    # S1 + S2 + S3 = S_in (1)
+    # S1*X1 + S2*X2 + S3*X3 = S_in*X_in (2)
 
-    def objetivo(Fs):
-        F1, F2, F3 = Fs
-        return x[1]*F2 + x[2]*F3 + 1e-4*(F1**2 + F2**2 + F3**2)
+    #Isolando o S3 da equação A
+    # S3 = S_in - S1 - S2 (3)
 
-    # -----------------------------
-    # Restrições de igualdade
-    # -----------------------------
-    def balanco_total(Fs):
-        return Fs.sum() - vazVinho_m3
+    # Substituindo (3) em (2)
+    # S1*X1 + S2*X2 + (S_in - S1 - S2)*X3 = S_in*X_in (4)
 
-    def balanco_etanol(Fs):
-        return np.dot(x, Fs) - vazVinho_m3 * fracEt_in
+    # Isolando S2 em (4)
+    # S2 = (S_in*X_in - S_in*X3 - S1*(X1 - X3)) / (X2 - X3) (5)
 
-    constraints = [
-        {"type": "eq", "fun": balanco_total},
-        {"type": "eq", "fun": balanco_etanol}
-    ]
+    # Substituindo (5) em (3)
+    # S3 = S_in - S1 - (S_in*X_in - S_in*X3 - S1*(X1 - X3)) / (X2 - X3) (6)
 
-    # -----------------------------
-    # Limites
-    # -----------------------------
-    bounds = [(0, None)] * 3
+    # ============ Prâmetros Conhecidos ============
 
-    # -----------------------------
-    # Chute inicial
-    # -----------------------------
-    x0 = np.array([10, 100, 157])
+    # S_in = vaz_in
+    # X_in = fracEt_in
+    # X1 = fracEt_Topo
+    # X2 = fracEt_Liq
+    # X3 = fracEt_Fundo
 
-    # -----------------------------
-    # Resolução
-    # -----------------------------
-    res = minimize(
-        objetivo,
-        x0,
-        method="SLSQP",
-        bounds=bounds,
-        constraints=constraints
-    )
+    # Média
+    def med (a):
+        return sum(a)/len(a)
+    
+    # Disponibilidade
+    disponibilidade = min(disponiagric/100, disponiClima/100, disponiIndust/100) * 24
+    
+    # Fração de Etanol nas Correntes
+    fracTotal = fracEt_Topo + fracEt_Liq + fracEt_Fundo
+    fracVapCalc = fracEt_Topo / fracTotal
+    fracLiqCalc = fracEt_Liq / fracTotal
+    fracFundoCalc = fracEt_Fundo / fracTotal
 
-    res.x = [float(x) for x in res.x]
+    if fracEt_Topo > 0 and fracEt_Liq > 0 and fracEt_Fundo > 0:
+        # Parâmetros da primeira Equação 
+        p1 = (vaz_in * fracEt_in - (vaz_in * fracFundoCalc))/(fracLiqCalc - fracFundoCalc)
+        p2 = (fracVapCalc - fracFundoCalc)/(fracLiqCalc - fracFundoCalc)
+    else:
+        return "Erro: Fração de Etanol inválida."
 
-    return {"nome": nome,
-            "Vazão do Topo (m³/h)": round(res.x[0], 2),
-            "Concentração do Topo": fracEt_Topo,
-            "Vazão do Líquido Intermediário (m³/h)": round(res.x[1], 2),
-            "Concentração do Líquido Intermediário": fracEt_Liq,
-            "Vazão do Fundo (m³/h)": round(res.x[2], 2),
-            "Concentração do Fundo": fracEt_Fundo,
-            "Etanol perdido (m³/h)": round(float(objetivo(res.x)),2)
+    S1, S2, S3 = None, None, None
+
+    S1_list, S2_list, S3_list = [], [], []
+
+    p3 = p2 - 1
+    p4 = vaz_in - p1
+
+    for i in range(1, int(vaz_in)):
+        S1 = i
+        S2 = p1 - p2 * S1
+        S3 = p3 * S1 + p4
+
+        if S2 > 0 and S3 > 0:
+            
+            S1_list.append(S1)
+            S2_list.append(S2)
+            S3_list.append(S3)
+
+            i += 0.5
+        else:
+            i += 0.5
+            continue
+
+    return {nome:
+                {
+                    "S1": round(med(S1_list), 2),
+                    "S2": round(med(S2_list), 2),
+                    "S3": round(med(S3_list), 2),
+                    "Retorno de etanol total (m³/h)": round(med(S1_list)*fracEt_Topo + med(S2_list)*fracEt_Liq,2) if nome=="Coluna AA1" else round(med(S1_list)*fracEt_Topo,2),
+                    "Etanol perdido (m³/h)": round(med(S3_list) * fracEt_Fundo,2) if nome=="Coluna AA1" else round(med(S2_list) * fracEt_Liq + med(S3_list) * fracEt_Fundo,2),
+                    "Balanço verificado (m³/h)": round(med(S1_list) + med(S2_list) + med(S3_list),2) - vaz_in,
+                    "Total Produzido (m³/h)": round(med(S1_list)*fracEt_Topo,2) * disponibilidade if nome=="Coluna B" else None
+                }
             }
 
-colunaAA1 = Coluna_PNL("Coluna AA1", vazVinho_m3, concentEt,
-                          fracEt_Topo=0.94, fracEt_Liq=0.05, fracEt_Fundo=0.01)
+colunaAA1 = Coluna_MN("Coluna AA1", vazVinho_m3, concentEt,
+                          fracEt_Topo=0.94, fracEt_Liq=0.05, fracEt_Fundo=0.01,
+                          disponiagric=100, disponiClima=100, disponiIndust=100)
 
-vazTopo_m3 = colunaAA1["Vazão do Topo (m³/h)"]
-vazLiq_m3 = colunaAA1["Vazão do Líquido Intermediário (m³/h)"]
+vazS1 = colunaAA1["Coluna AA1"]["S1"]
+vazS2 = colunaAA1["Coluna AA1"]["S2"]
+vazTotal_colB = vazS1 + vazS2
+fracEt_in_colB = (vazS1 * 0.94 + vazS2 * 0.05) / (vazTotal_colB)
 
-vazTotal = vazTopo_m3 + vazLiq_m3
-concentTotal = (vazTopo_m3 * colunaAA1["Concentração do Topo"] + vazLiq_m3 * colunaAA1["Concentração do Líquido Intermediário"]) / vazTotal
-
-colunaB = Coluna_PNL("Coluna B", vazTotal, concentTotal, fracEt_Topo=0.95, fracEt_Liq=0.04, fracEt_Fundo=0.01)
+colunaB = Coluna_MN("Coluna B", vazTotal_colB, fracEt_in_colB,
+                         fracEt_Topo=0.95, fracEt_Liq=0.04, fracEt_Fundo=0.01,
+                         disponiagric=100, disponiClima=100, disponiIndust=100)
 
 print(MistResult)
 print(ferment)
 print(colunaAA1)
 print(colunaB)
-print(colunaB["Vazão do Topo (m³/h)"] * 24)
+print(colunaB["Coluna B"]["Total Produzido (m³/h)"])
